b110511755686ee7b7ab81da3bd178d449d0eeaa
ECrowe6593 <eqc5518@psu.edu>
2024-02-17 00:04:51 -0500

    Started making counter app

--- src/counter-app.js
+++ src/counter-app.js
@@ -20,14 +20,77 @@ export class CounterApp extends LitElement {
 
   static get styles() {
     return css`
+    :host {
+      display: block;
+      margin: 16px;
+    }
+
+    .count-card {
+      background-color: gray;
+      border-radius: 10%;
+      width: 200px;
+      height: 200px;
+      padding: 16px;
+    }
+
+    .number-container {
+      background-color: blue;
+      width: 50%;
+      height: 45%;
+      margin: auto;
+    }
+
+    .button-container {
+      display: block;
+      background-color: black;
+      width: 100%;
+      height: 50%;
+    }
+
+    .minus {
+      background-color: #ff6865;
+      display: block;
+      width: 30%;
+      height: 50%;
+      float: left;
+      
+    }
+
+    .plus {
+      background-color: #39e75f;
+      display: block;
+      width: 30%;
+      height: 50%;
+      float: right;
+    }
+
+    .minus:focus,
+    .minus:hover {
+      background-color: red;
+    }
+
+    .plus:focus,
+    .plus:hover {
+      background-color: green;
+    }
+
+
     `;
   }
 
 
   render() {
     return html`
+      <div class="count-card">
+        <div class="number-container">
+          
+        </div>
 
-      <div>Hello World</div>
+        <div class="button-container">
+            <button class="minus">-</button>
+            <button class="plus">+</button>
+        </div>
+      </div>
     `;
   }
 

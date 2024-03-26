7f77899fcb8d7d173e4b837655ad146bab99ad90
ECrowe6593 <eqc5518@psu.edu>
2024-03-26 13:29:59 -0400

    more css and rendering

--- src/haxcms-char.js
+++ src/haxcms-char.js
@@ -33,15 +33,46 @@ export class haxcms extends DDD {
         }
 
         .search {
+          background-color: var(--ddd-theme-default-slateMaxLight);
           font-family: "Press Start 2P", system-ui;
+          min-width: var(--haxcms-party-ui-searchbar, 150px);
+          margin: var(--ddd-spacing-3);
+          padding: var(--ddd-spacing-6);
+        }
+
+        .button {
+          color: blue;
+          background-color: var(--ddd-theme-default-roarMaxLight);
+          border: var(--ddd-border-sm);
+          border-color: var(--ddd-theme-default-nittanyNavy);
+          font-family: "Press Start 2P", system-ui;
+          font-size: var(--ddd-font-size-3xs);
+          font-weight: 500;
           min-width: 150px;
+          margin: var(--ddd-spacing-3);
+          padding: var(--ddd-spacing-5);
+        }
+
+        .button:hover {
+          background-color: var(--ddd-theme-default-nittanyNavy);
+          color: var(--ddd-theme-default-roarMaxLight);
+          transform: scale(1.1);
+          transition: all 300ms ease-in-out;
         }
     `];
   }
 
   render() {
     return html`
-      <div class="container"></div>
+      <div class="container">
+        <div class="button-container">
+          <input
+            type="text"
+            class="search-input"
+            placeholder="Search Party Member"
+            />
+        </div>
+      </div>
   `;
   }
 
